1a48372798e81d9b9808517dfdf72dce732362d6
OlehEngineer <73915121+OlehEngineer@users.noreply.github.com>
2022-01-22 18:28:05 +0200

    package name change
    package name change

--- bubbleSort.go
+++ bubbleSort.go
@@ -1,16 +1,7 @@
-package main
+package bubbleSort
 
-import "fmt"
-
-func main() {
-
-	var forSort = []int{3, 7, 4, 1, 8, 2, 3, 9, 32, 12, 5, 56, 18}
-	fmt.Printf("your slice => %v before sorting\n", forSort)
-	fmt.Printf("your slice => %v after sorting\n", Sorting(forSort))
-	fmt.Printf("your slice => %v after reverse sorting", ReverseSort(forSort))
-}
 func Sorting(data []int) []int {
-	//standard bubble sorting algorithm. From small to bigger
+	//standard bubble sorting algorithm. Sort slice of integers from small to bigger
 	var length int = len(data) // variable equal slice size for looping through all items in the slice
 
 	for k := 1; k <= length; k++ { // loop for checking order of number as many times as items in the slice
@@ -29,7 +20,7 @@ func Sorting(data []int) []int {
 	return data
 }
 func ReverseSort(data []int) []int {
-	//standard bubble sorting algorithm. From bigger to small
+	//standard bubble sorting algorithm. Sort slice of integers from bigger to small
 	var length int = len(data) // variable equal slice size for looping through all items in the slice
 
 	for k := 1; k <= length; k++ { // loop for checking order of number as many times as items in the slice
